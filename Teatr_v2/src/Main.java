e1316482126131a355a69f5c3f3b92d876a24e7d
KamilTPWR <277985@student.pwr.edu.pl>
2024-10-10 14:33:07 +0200

    Relase 1.0.0

---
+++ Teatr_v2/src/Main.java
@@ -0,0 +1,42 @@
+import teatr.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating instances of Spectacle
+        Spectacle spectacle1 = new Spectacle("Hamlet", 15, 10, 2024, 100);
+        Spectacle spectacle2 = new Spectacle("Macbeth", 20, 10, 2024, 150);
+        Spectacle spectacle3 = new Spectacle("Romeo and Juliet", 25, 10, 2024, 200);
+
+        // Displaying information about each spectacle
+        System.out.println("Spectacle Information:");
+        spectacle1.showInfo();
+        System.out.println();
+        spectacle2.showInfo();
+        System.out.println();
+        spectacle3.showInfo();
+        System.out.println();
+
+        // Creating tickets for spectacles
+        Ticket ticket1 = new Ticket(spectacle1, 10); // Seat 10 for Hamlet
+        Ticket ticket2 = new Ticket(spectacle2, 20); // Seat 20 for Macbeth
+        Ticket ticket3 = new Ticket(spectacle3, 15); // Seat 15 for Romeo and Juliet
+
+        // Displaying ticket information
+        System.out.println("Ticket Information:");
+
+        ticket1.showInfo();
+        System.out.println();
+
+        ticket2.showInfo();
+        System.out.println();
+
+        ticket3.showInfo();
+        System.out.println();
+
+        // Attempting to create a ticket for an already taken seat
+        Ticket ticket4 = new Ticket(spectacle1, 10); // Attempt to use the same seat for Hamlet
+
+        // Displaying total tickets created
+        System.out.println("Total Tickets Created: " + Ticket.getTicketCounter());
+    }
+}
--- Teatr_v2/src/teatr/Ticket.java
+++ Teatr_v2/src/teatr/Ticket.java
@@ -1,7 +1,13 @@
 package teatr;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Random;
+
 public class Ticket {
     //Struct
+    private static final Set<Integer> existingTicketIds = new HashSet<>();
+    private static final Set<Integer> existingSeatNumbers = new HashSet<>();
     private static int ticketCounter = 0;
     private int ticketId;
     private Spectacle spectacle;
@@ -9,19 +15,40 @@ public class Ticket {
 
     //Constructor
     public Ticket(Spectacle spectacle, int seatNumber) {
-        this.ticketId = ++ticketCounter;
+        do {
+            ticketId = generateUniqueTicketId();
+        } while (existingTicketIds.contains(ticketId));
+
+        existingTicketIds.add(ticketId);
+
+        // Ensure the seat number is unique
+        if (existingSeatNumbers.contains(seatNumber)) {
+            System.out.println("Seat number " + seatNumber + " is already taken. Please choose a different seat.");
+            return; // Exit constructor without creating a ticket
+        }
+
+        existingSeatNumbers.add(seatNumber);
+
         this.spectacle = spectacle;
         this.seatNumber = seatNumber;
+        ticketCounter++;
     }
 
-    public int getTicketId() {
-        return ticketId;
+    private int generateUniqueTicketId() {
+        Random random = new Random();
+        return 1000000000 + random.nextInt(900000000); // Generates a number between 1000000000 and 1999999999
     }
-    public Spectacle getSpectacle() {
-        return spectacle;
-    }
+
+//    public Spectacle getSpectacle() {
+//        return spectacle;
+//    }
+
     public int getSeatNumber() {
         return seatNumber;
+    }
+
+    public static int getTicketCounter() {
+        return ticketCounter;
     }
 
     public void showInfo() {
