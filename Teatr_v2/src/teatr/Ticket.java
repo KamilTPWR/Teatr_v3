42adaab3f366cd368e2fd9fd6aaca9709aa11bd4
KamilTPWR <277985@student.pwr.edu.pl>
2024-10-10 13:42:12 +0200

    wywalilem test

--- Teatr_v2/src/teatr/Ticket.java
+++ Teatr_v2/src/teatr/Ticket.java
@@ -1,37 +1,32 @@
 package teatr;
 
 public class Ticket {
-    private static int ticketCounter = 0; // Licznik biletów do unikalnego ID
+    //Struct
+    private static int ticketCounter = 0;
     private int ticketId;
     private Spectacle spectacle;
     private int seatNumber;
 
-    // Konstruktor
+    //Constructor
     public Ticket(Spectacle spectacle, int seatNumber) {
-        this.ticketId = ++ticketCounter; // Zwiększanie licznika i przypisanie ID
+        this.ticketId = ++ticketCounter;
         this.spectacle = spectacle;
         this.seatNumber = seatNumber;
     }
 
-    // Metoda do wyświetlenia informacji o bilecie
-    public void displayInfo() {
-        System.out.println("Ticket ID: " + ticketId);
-        System.out.println("Spectacle: " + spectacle.getTitle());
-        System.out.println("Seat Number: " + seatNumber);
-    }
-
-    // Getter dla ticketId
     public int getTicketId() {
         return ticketId;
     }
-
-    // Getter dla spektaklu
     public Spectacle getSpectacle() {
         return spectacle;
     }
-
-    // Getter dla seatNumber
     public int getSeatNumber() {
         return seatNumber;
+    }
+
+    public void showInfo() {
+        System.out.println("Ticket ID: " + ticketId);
+        System.out.println("Spectacle: " + spectacle.getTitle());
+        System.out.println("Seat Number: " + seatNumber);
     }
 }
